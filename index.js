4606aac32a17311dd531f794b24e40d8731f166e
nd31pr0 <ndzinfor@gmail.com>
2022-06-08 02:26:00 +0100

    Adjustment made to clear input field once 'save input' button is clicked.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const inputBtn = document.getElementById('input-btn')
-let myLeads = ['val1', 'val2', 'val3']
+let myLeads = []
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
-let listItems = ""
+
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
-  console.log(myLeads)
+  inputEl.value = ""
+  renderLeads()
 })
-
-for (let i = 0; i < myLeads.length; i++) {
-  listItems += "<li>" + myLeads[i] + "</li>"
+function renderLeads() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li><a target ='_blank' href ='" + myLeads[i] + "' >" + myLeads[i] + "</li>"
+  }
+  ulEl.innerHTML = listItems
 }
-ulEl.innerHTML = listItems
