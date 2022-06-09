1f9e40bb28f137e07ae8ce40cce5e66d34d3ccfc
nd31pr0 <ndzinfor@gmail.com>
2022-06-09 12:43:41 +0100

    Developments done for deploying the partial extension in chrome

---
+++ Leads tracker/index.js
@@ -0,0 +1,23 @@
+const inputBtn = document.getElementById('input-btn')
+let myLeads = []
+const inputEl = document.getElementById('input-el')
+const ulEl = document.getElementById('ul-el')
+
+
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  renderLeads()
+})
+function renderLeads() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+    <li>
+      <a target ='_blank' href ='${myLeads[i]}' >${myLeads[i]}
+      </a>
+    </li>`
+  }
+  ulEl.innerHTML = listItems
+}
