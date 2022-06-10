95986adfcd530f15b1f536ffe616d2e5d8eeb8c8
nd31pr0 <ndzinfor@gmail.com>
2022-06-10 04:09:27 +0100

    Created a delete button for the leads and implemented its logic

--- Leads tracker/index.js
+++ Leads tracker/index.js
@@ -2,22 +2,38 @@ const inputBtn = document.getElementById('input-btn')
 let myLeads = []
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
+const deleteBtn = document.getElementById('delete-btn')
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem((myLeads)))
 
-
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
   renderLeads()
-})
-function renderLeads() {
+}
+
+function render(leads) {
   let listItems = ""
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li>
-      <a target ='_blank' href ='${myLeads[i]}' >${myLeads[i]}
+      <a target ='_blank' href ='${leads[i]}' >${leads[i]}
       </a>
     </li>`
   }
   ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+console.log(leadsFromLocalStorage)
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value)
+  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  render(myLeads)
+})
+
