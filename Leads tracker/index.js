112094e51b076038854c0a1542e151425dd11fcc
nd31pr0 <ndzinfor@gmail.com>
2022-06-10 23:11:08 +0100

    Created the 'save tab' button to save the current url in our browser

--- Leads tracker/index.js
+++ Leads tracker/index.js
@@ -3,13 +3,22 @@ let myLeads = []
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 const deleteBtn = document.getElementById('delete-btn')
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem((myLeads)))
+const tabBtn = document.getElementById('tab-btn')
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
   renderLeads()
 }
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+  })
+
+})
 
 function render(leads) {
   let listItems = ""
